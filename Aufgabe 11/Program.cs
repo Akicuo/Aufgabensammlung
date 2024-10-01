74fc8792a2eab1df72a3bc7e6c3219d073d159cf
Akicuo <user.of.replit@gmail.com>
2024-10-01 14:07:32 +0200

    qgqgwhw

---
+++ Aufgabe 11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aufgabe 11 : Eine diagonale Linie zeichnen");
+            Console.WriteLine("**************************************************\n");
+
+            Console.WriteLine("Wie lang soll die Linie sein?");
+            int n = int.Parse(Console.ReadLine());
+
+            int columns = n;
+            int row_length = columns;
+            for (int a = 0; a < columns; a++)
+            {
+                for (int i = 0; i < row_length; i++)
+                {
+                    if (i != a)
+                    { 
+                    Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+
+                }
+                Console.Write("\n");
+            }
+
+        }
+        
+    }
+}
---
+++ Aufgabe12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool firstIteration = true;
+
+            while (firstIteration || i >= 0)
+            {
+                doSomething();
+
+                firstIteration = false;
+            }
+            //I Variable sonst vor der Schleife Initiieren und DoSomething in den gleichen Klassen als statische Funktion definieren//
+
+        }
+    }
+}
