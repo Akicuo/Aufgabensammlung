30734219135e892a525cf1c7694493fd2c60b7e7
Akicuo <user.of.replit@gmail.com>
2024-10-01 14:33:04 +0200

    qage

---
+++ Aufgabe 13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aufgabe_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aufgabe 13 : Schaltjahr");
+            Console.WriteLine("**************************************************\n");
+
+            Console.Write("Jahr eingeben: ");
+            if (int.TryParse(Console.ReadLine(), out int jahr))
+            {
+                bool isLeapYear = (jahr % 4 == 0 && jahr % 100 != 0) || (jahr % 400 == 0);
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine("Das ist ein Schaltjahr.");
+                }
+                else
+                {
+                    Console.WriteLine("Das ist kein Schaltjahr.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Das ist kein Jahr (Eingabefehler)");
+            }
+        }
+    }
+}
