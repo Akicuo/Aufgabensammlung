fc8cc0b692ca60fbdd515a22ac9ee248f6dd1269
Akicuo <user.of.replit@gmail.com>
2024-10-01 07:36:40 +0200

    ttjr

--- Aufgabe 6/Program.cs
+++ Aufgabe 6/Program.cs
@@ -12,17 +12,18 @@ namespace Aufgabe_6
         {
             Console.WriteLine("Aufgabe 6 : Kleines 1x1");
             Console.WriteLine("**************************************************\n");
-            int current_row = 0; int current_column = 0;
-            int f = 10;
-            for (int i =current_column; i != f; i++)
+            int current_row = 0; int current_column = 1;
+            int num = 1;
+            int f = 11;
+            for (int i =current_column; i != f ; i++)
             {
-                for (int b = current_column; b != f; b++)
+                for (int b = current_row + num; b != f; b++)
                 {
-                    Console.Write($"{b}\t");
+                    int current_num = b * i;
+                    Console.Write($"{current_num}\t");
                 }
                 Console.Write("\n");
-                // noch nicht fertig //
-                // noch nicht fertig //
+                
             }
 
         }
