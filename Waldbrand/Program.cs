4f7dd82f21055319e6562d0fe3238d09e24b03bf
Akicuo <user.of.replit@gmail.com>
2024-10-02 14:45:42 +0200

    2241

--- Waldbrand/Program.cs
+++ Waldbrand/Program.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Waldbrand
+namespace Memory
 {
     internal class Program
     {
@@ -14,10 +14,52 @@ namespace Waldbrand
             Console.WriteLine("MEMORY -> Hinter den '?' verstecken sich Symbole, die paarweise vorkommen. Finden Sie diese!");
             Console.WriteLine("Zum Aufdecken wählen Sie zwei Positionen in der Form: Zeile1Spalte1Zeile2Spalte2.");
             Console.WriteLine("Z.Bsp.: 2142 deckt das Symbol in Zeile 2 u. Spalte 1 auf sowie das Symbol in Zeile 4 u. Spalte 2.\n");
-            string[] board = { "11", "open" };
+
+            string[,] card_list = {
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-" }
+    };
+            string[,] flipped = {
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-"},
+        { "-", "-", "-", "-" }
+    };
+            string[,] filled_icon_board = FillBoard(card_list);
+
+
+
+
+
+
+
+
 
             // ♬ ☢ ⌨ ❒  〄 🦴 💵  🐻‍❄//
         }
-        static void DrawBoard()
+        static string[,] FillBoard(string[,] board)
+        {
+            string[] icons = { "♬", "☢", "⌨", "❒", "〄", "🦴", "💵", "🐻" };
+            int[] icons_left = { 2, 2, 2, 2, 2, 2, 2, 2 };
+            for (int row = 0; row < 5; row++) // fill row <->
+            {
+
+                for (int column = 0; column < 5; column++) // fill column |
+                {
+                    board[row, column] = "x";
+
+
+                }
+
+            }
+        }
+        static int GenRandomNum(int min, int max)
+        {
+            Random rnd = new Random(); // Von Stackoverflow.com //
+            int randint = rnd.Next(min, max+1); // Von Stackoverflow.com //
+            return randint;
+        }
     }
 }
